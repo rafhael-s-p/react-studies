024a652bd02a97102c2d2cc4c733e1ab338db693
Rafhael <rafhael.dev@gmail.com>
2024-11-25 09:36:57 -0300

    feat: react properties

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,25 +1,20 @@
-const repositoryName = "dynamic name";
+import {RepositoryItem} from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform',
+}
 
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Repository List</h1>
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Respository Acess
-                    </a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Respository Acess
-                    </a>
-                </li>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     )
