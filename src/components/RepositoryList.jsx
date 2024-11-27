7187b3e8ff61441a8a30684a90f6b7fc267e809c
Rafhael <rafhael.dev@gmail.com>
2024-11-27 09:09:09 -0300

    feat: use effect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import {RepositoryItem} from "./RepositoryItem";
 import '../styles/repositories.scss';
+import {useState, useEffect} from "react";
 
 const repository = {
     name: 'unform',
@@ -8,6 +9,17 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/rafhael-s-p/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+    console.log(repositories)
+
     return (
         <section className="repository-list">
             <h1>Repository List</h1>
