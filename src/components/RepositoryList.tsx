851c31d39647bc7a2393a3f1bfa30866dde09000
Rafhael <rafhael.dev@gmail.com>
2024-12-03 10:20:48 -0300

    refactor: react typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,9 +2,15 @@ import {RepositoryItem} from "./RepositoryItem";
 import '../styles/repositories.scss';
 import {useEffect, useState} from "react";
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
 
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/rafhael-s-p/repos')
@@ -18,7 +24,7 @@ export function RepositoryList() {
             <ul>
                 {
                     repositories.map(repository => {
-                        return <RepositoryItem key={repository.id} repository={repository}/>
+                        return <RepositoryItem key={repository.name} repository={repository}/>
                     })
                 }
             </ul>
